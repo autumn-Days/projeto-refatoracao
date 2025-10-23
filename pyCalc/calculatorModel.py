5a2aff368691e4a040cc3a4e874de9fa6a126c1b
gabrieldiasjit <gabrieldiasjittechnology@gmail.com>
2025-10-23 12:00:14 -0300

    melhoria no controller

--- pyCalc/calculatorModel.py
+++ pyCalc/calculatorModel.py
@@ -26,32 +26,51 @@ class CalculatorModel():
                                 ('^2', 5, 5, "**2")]
 
         self.other_btns = [ ('AC', 5, 0), ('=', 5, 2), 
-                            ('<-', 2, 5), ('x!', 3, 5),]
+                            ('<-', 2, 5), ('x!', 3, 5)]
 
         self.button_list = {}
         self.display = None
 
     def _init_ui(self):
         for button, y, x in self.number_btns :
-            self.button_list[button] = tk.Button(self, text=button, command=lambda btn=button: self.get_variables(int(btn)), font=self.FONT_LARGE)
+            parameters = (button)
+            self.button_list[button] = self.create_button(parameters, number=True)
 
-        for (button, y, x, op) in self.operation_btns :
-            self.button_list[button] = tk.Button(self, text=button, command=lambda v=op : self.get_operation(v), font=self.FONT_LARGE)
+        for button, y, x, op in self.operation_btns :
+            parameters=(button,op)
+            self.button_list[button] = self.create_button(parameters,operation=True)
 		
-        for (button, y, x) in self.other_btns :
+        for button, y, x in self.other_btns :
             if button == "AC" :
-                self.button_list[button] = tk.Button(self, text=button, command=self.clear_all,
-					                        font=self.FONT_LARGE, foreground="red")
+                parameters = (button,"red")
+                self.button_list[button] = self.create_button(parameters,custom=button)
             elif button == "=" :
                 self.button_list[button] = tk.Button(self, text=button, command=self.calculate,
 					                        font=self.FONT_LARGE, foreground="red")
             elif button == "x!" :
                 self.button_list[button] = tk.Button(self, text="x!", command= lambda v="!": self.factorial(v),
 					                        font=self.FONT_LARGE)
-            elif button == "'<-" :
+            elif button == "<-" :
                 self.button_list[button] = tk.Button(self, text="<-", command= self.undo,
 					                        font=self.FONT_LARGE)
 
+    def create_button(self,parameters,number=False,operation=False, custom=None):
+        if (number):
+            button =parameters[0]
+            return tk.Button(self, text=button, command=lambda btn=button: self.get_variables(int(btn)), font=self.FONT_LARGE)
+        elif (operation):
+            button, op = parameters
+            return tk.Button(self, text=button, command=lambda v=op : self.get_operation(v), font=self.FONT_LARGE)
+        elif (custom):
+            if custom == "AC":
+               buttom, color = parameters
+               return tk.Button(self, text=button, command=self.clear_all,font=self.FONT_LARGE, foreground=color)
+            elif custom == "=":
+                pass
+            elif custom == "x!":
+                pass
+            elif custom == "<-":
+                pass
     def get_variables(self, num):
         """Gets the user input for operands and puts it inside the entry widget.
         If a new operation is being carried out, then the display is cleared.
@@ -66,7 +85,6 @@ class CalculatorModel():
         length = len(operator)
         self.display.insert(self.i, operator)
         self.i += length
-
 
     def clear_all(self, new_operation=True):
         """clears all the content in the Entry widget."""
